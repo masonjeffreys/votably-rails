5413c7306b6e68ab8fa16355495532a5fd4d6059
Jeff Mason <masonjeffreys@gmail.com>
2016-07-26 15:17:39 -0400

    AddLoaderVerificationToPublicFolder

--- app/controllers/verify_controller.rb
+++
@@ -1,3 +0,0 @@
-class VerifyController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
-  get '/loaderio-301b9be9103044fa7c6d67e94b308dc7', to: 'verify#show'
-
   get '/public/:id',   to: 'public_views#show', as: "public"
   get '/respond/:id', to: 'poll_responses#new', as: "respond"
 
